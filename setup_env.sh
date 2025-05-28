8ae970f03d01afcac36336f90db000bd7d472cab
Alon <alonof27@gmail.com>
2025-05-28 16:27:58 +0300

    Starting a setup script

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+
+# cp relevant files from dotfiles to ~
+# .zshrc .vimrc .vim/ .node.yaml .tmux.conf
+# install oh-my-zsh
+# install install plugins
+# install lsp
+# source ./zshrc
